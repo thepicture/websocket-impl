8c5195959e7a33410a34e070b943c092c6a3d3ec
thepicture <85333076+thepicture@users.noreply.github.com>
2023-02-11 23:35:09 -0500

    feat(WebSocket): add command execution

--- src/ws/WebSocket.test.js
+++ src/ws/WebSocket.test.js
@@ -3,6 +3,9 @@ const { WebSocket } = require("./WebSocket");
 const COMMAND1 = "time";
 const RESPONSE1 = "08:00";
 
+const COMMAND2 = "echo $((42))";
+const RESPONSE2 = "42";
+
 describe("WebSocket", () => {
   let websocket;
 
@@ -10,13 +13,26 @@ describe("WebSocket", () => {
     websocket = new WebSocket();
 
     websocket.addCommand(COMMAND1, RESPONSE1);
+    websocket.addCommand(COMMAND2, RESPONSE2);
   });
 
   afterEach(() => {
     websocket = null;
   });
 
-  it("when given time command then returns 08:00", () => {
-    expect(websocket._getResponse(COMMAND1)).toBe(RESPONSE1);
+  it("when given time command then returns 08:00", async () => {
+    const expected = RESPONSE1;
+
+    const actual = await websocket._getResponse(COMMAND1);
+
+    expect(actual).toBe(expected);
+  });
+
+  it("when registered command is 'echo $((42))' then returns 42", async () => {
+    const expected = RESPONSE2;
+
+    const actual = (await websocket._getResponse(COMMAND2)).slice(0, -1);
+
+    expect(actual).toBe(expected);
   });
 });
